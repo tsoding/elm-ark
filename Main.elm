9ead97e73b76fafe4f76ec33bb309db8c0e98dae
rexim <reximkut@gmail.com>
2017-02-22 22:41:58 +0600

    Implement the arkanoid ball mechanic

--- Main.elm
+++ Main.elm
@@ -13,26 +13,42 @@ main = Html.program { init = init
 
 type alias Model = { x : Float
                    , y : Float
-                   , screenWidth : Int
-                   , screenHeight : Int
+                   , dx : Float
+                   , dy : Float
+                   , radius : Float
+                   , screenWidth : Float
+                   , screenHeight : Float
                    }
 
 init : (Model, Cmd Msg)
 init = ({ x = 60.0
         , y = 60.0
-        , screenWidth = 800
-        , screenHeight = 600
+        , dx = 2.0
+        , dy = 2.0
+        , radius = 10.0
+        , screenWidth = 400
+        , screenHeight = 300
         }, Cmd.none)
 
 -- UPDATE
 
 type Msg = Tick Time
 
+moveBall : Model -> Model
+moveBall model = { model | x = model.x + model.dx
+                         , y = model.y + model.dy }
+
+bounceBall : Model -> Model
+bounceBall model = { model | dx = if model.x - model.radius <= 0 || model.x + model.radius >= model.screenWidth
+                                  then -model.dx
+                                  else model.dx
+                           , dy = if model.y - model.radius <= 0 || model.y + model.radius >= model.screenHeight
+                                  then -model.dy
+                                  else model.dy }
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update _ model =
-    let velocity = 1.0
-    in 
-    ({ model | x = model.x + velocity, y = model.y + velocity}, Cmd.none)
+    (model |> moveBall |> bounceBall, Cmd.none)
 
 -- SUBSCRIPTIONS
 
@@ -46,5 +62,5 @@ view model =
     svg [ width (toString model.screenWidth), height (toString model.screenHeight)]
         [ circle [ cx (toString model.x)
                  , cy (toString model.y)
-                 , r "50", Svg.Attributes.style "fill:red" ]
+                 , r (toString model.radius), Svg.Attributes.style "fill:red" ]
               []]
